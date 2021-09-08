f92e5330623f2559ca772637ac4f7ec83f8387f4
Felipe Libório <felipeliborio@outlook.com>
2021-09-08 07:34:37 -0300

    list operations

--- src/services/OperationsLogService.ts
+++ src/services/OperationsLogService.ts
@@ -8,10 +8,30 @@ interface IList {
 
 class OperationsLogService {
   async list({ userId, fromDate, toDate }: IList) {
-    return database('users').select('*');
+    return new Promise((resolve, reject) => {
+      const query = database('operations_log as ol')
+      .leftJoin('users', 'ol.user_id', 'users.id')
+      .select(['users.username', 'ol.*' ]);
+
+      if (userId != undefined) {
+        query.where({user_id: userId});
+      }
+
+      if (fromDate) {
+        query.where('created_at', '>=', fromDate);
+      }
+
+      if (toDate) {
+        query.where('created_at', '<=', toDate);
+      }
+
+      query.then(operations => {
+        resolve({ status: 1, operations });
+      });
+    });
   }
 
-  async make(operation: string) {
+  async make(operation: string, ) {
 
   }
 }
